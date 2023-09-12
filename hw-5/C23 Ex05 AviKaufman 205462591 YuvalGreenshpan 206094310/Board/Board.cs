541a366c3f94008109a0601afc8e4afceb6c98be
Avi Kaufman <avrham.kaufman@post.idc.ac.il>
2023-09-12 17:06:05 +0300

    first commit

---
+++ hw-5/C23 Ex05 AviKaufman 205462591 YuvalGreenshpan 206094310/Board/Board.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace Ex02
+{
+    public class Board
+    {
+        private SecretCode m_SecretCode;
+        private List<Guess> m_PlayerGuesses;
+        private List<List<char>> m_FeedbackOnGuesses;
+
+        public Board(char[] i_CharArrayToGenerateSecretCodeFrom, int i_SizeOfSecretCode)
+        {
+            m_SecretCode = new SecretCode(i_CharArrayToGenerateSecretCodeFrom, i_SizeOfSecretCode);
+            m_PlayerGuesses = new List<Guess>();
+            m_FeedbackOnGuesses = new List<List<char>>();
+        }
+
+        public SecretCode SecretCode
+        {
+            get { return this.m_SecretCode; }
+        }
+
+        public List<Guess> PlayerGuesses
+        {
+            get { return this.m_PlayerGuesses; }
+        }
+
+        public List<List<char>> FeedbackOnGuesses
+        {
+            get { return this.m_FeedbackOnGuesses; }
+        }
+        public void AddGuess(Guess i_Guess)
+        {
+            m_PlayerGuesses.Add(i_Guess);
+            m_FeedbackOnGuesses.Add(i_Guess.GetFeedbackOnGuess(m_SecretCode));
+        }
+    }
+}
